119895b92d75df5d6283c634d3b45e59ed94245f
Lucas Medina <lucasmedinacav@hotmail.com>
2017-07-18 00:05:30 -0300

    ajustes heroku

--- manual-deploy.sh
+++ manual-deploy.sh
@@ -1,1 +1,1 @@
-web java -Dserver.port=$PORT $JAVA_OPTS -jar target/app.jar
+heroku deploy:jar -i worker-service/target/worker-service-0.0.1-SNAPSHOT.jar -j web-service/target/web-service-0.0.1-SNAPSHOT.jar --app exampledriven-springboot-dev
